a5dc39598001392ef11b4afc1eb0fd74a6fe5b50
sonicx180 <83236320+sonicx180@users.noreply.github.com>
2021-07-08 14:40:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ userinput = input("Would you like to create,read, or write a file? (c , r , w)
 if userinput == 'c':
   # I'll put this code in a try statement incase the user already has a file with this name
   try:
+    
+       
+              
+                  
+                  
+  
                   
                 
     
