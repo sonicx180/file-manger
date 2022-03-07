9255b1abd8e69886890507a2eac0f79709fad74b
sonicx180 <83236320+sonicx180@users.noreply.github.com>
2022-03-06 19:03:00 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 
-userinput = input("Would you like to create,read, or write a file? (c , r , w)
+userinput = input("Would you like to create,read, or write a file? (c , r , w)")
 
 if userinput == 'c':
   # I'll put this code in a try statement incase the user already has a file with this name
   try:
+    name = input("What should be the name of the file ? (With the .filetype)")
     
+    file = open(name,"x")
+    
+    print(" A file called " + name + "was created!") 
+    
+  except: 
+    print("There was an error creating the file")
+ 
        
               
                   
